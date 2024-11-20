5232f1d407e138f2fb82d23a71d3a45dba11a7c2
Eric Sorum <esorum@sgcmail.com>
2024-11-20 10:52:21 -0600

    Updates

--- src/components/Header.tsx
+++
@@ -1,9 +0,0 @@
-import { PageType } from '../lib/definitions'
-
-export default function Header({ title }: PageType) {
-  return (
-    <div className="flex flex-col items-start justify-end h-32 max-w-screen-lg w-full">
-     <h1 className="text-4xl text-white">{title}</h1>
-    </div>
-  )
-}
--- src/components/LowerLeftText.tsx
+++
@@ -1,7 +0,0 @@
-export default function LowerLeftText() {
-  return (
-    <div className="absolute bottom-[10px] left-[10px] text-right text-white shadow-slate-200 text-3xl">
-      See your vision come to life.
-    </div>
-  )
-}
