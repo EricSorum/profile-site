fd4e0362fbbe9a41b815aa21aecc07632e494101
Eric Sorum <esorum@sgcmail.com>
2024-11-26 13:40:54 -0600

    Added img zoom on hover.

--- src/components/Feature.tsx
+++ src/components/Feature.tsx
@@ -3,37 +3,34 @@ import { FeatureType } from "../lib/definitions";
 
 export default function Feature(props: FeatureType) {
   const [ active, isActive ] = useState(false)
-  const activeClasses = active ? [
-    'bg-stone-500/[0.2]'
-  ] : '';
   const alignment = props.id % 2;
   const alignmentClass = alignment ? 'flex-row-reverse' : '';
   return (
     <div
     className=""
     >
-      {/* <div> */}
         <div 
-          onMouseEnter={()=> isActive(true)}
-          onMouseLeave={()=> isActive(false)}      
           className={`
-            ${activeClasses}
             ${alignmentClass}
             lg:flex items-center gap-10 p-20
           `}>
-        <img 
-            src={props.img} 
-
-            className={`
-              lg:w-1/2
-            `}
-          />
-          <div className="">
+          <div className="lg:w-1/2">
+            <div className="overflow-hidden relative hover:bg-stone-200/[0.2]">
+              <img
+                src={props.img}
+                className={`
+                  transition-transform ease-in-out duration-300 
+                  hover:scale-90
+                 object-cover
+                `}
+              />
+            </div>
+          </div>
+          <div className="lg:w-1/2">
             <h3 className="text-xl">{props.title}</h3>
             <p>{props.txt}</p>
           </div>
         </div>
-      {/* </div> */}
     </div>
   )
 }
