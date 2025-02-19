8738e4675d4e71ad58c6bb1d38b9e27a35f940a7
Eric Sorum <ejsorum@gmail.com>
2025-02-19 10:23:37 -0600

    Rearranged Feature size, alignment.

--- src/components/Feature.tsx
+++ src/components/Feature.tsx
@@ -10,7 +10,7 @@ export default function Feature(props: FeatureType) {
             ${alignmentClass}
             lg:flex items-center gap-10 p-20
           `}>
-          <div className="lg:w-1/2">
+          <div className="lg:w-2/3">
             <div className="overflow-hidden relative hover:bg-stone-200/[0.2]">
               <img
                 src={props.img}
@@ -18,44 +18,44 @@ export default function Feature(props: FeatureType) {
                   transition-transform ease-in-out duration-300 
                   hover:scale-90
                   object-cover
+                  mb-4
                 `}
               />
             </div>
-          </div>
-          <div className="lg:w-1/2">
             <h3 className="text-xl">{props.title}</h3>
             <div className="">
-            <button className="
-            m-2 p-1 
-            outline inline-block outline-slate-100 rounded-lg 
-            hover:bg-stone-200/[0.1] hover:shadow-xl
-            opacity-70 
-            hover:opacity-100 
-            transition ease-in-out
-            text-lg
-            ">Option</button>
-
-<button className="
-            m-2 p-1 
-            outline inline-block outline-slate-100 rounded-lg 
-            hover:bg-stone-200/[0.1] hover:shadow-xl
-            opacity-70 
-            hover:opacity-100 
-            transition ease-in-out
-            text-lg
-            ">Option</button>
-                        <button className="
-            m-2 p-1 
-            outline outline-slate-100 rounded-lg 
-            hover:bg-stone-200/[0.1] hover:shadow-xl
-            opacity-70 
-            hover:opacity-100 
-            transition ease-in-out
-            text-lg
-            ">Option</button>
-              
+              <button className="
+                m-2 p-1 
+                outline inline-block outline-slate-100 rounded-lg 
+                hover:bg-stone-200/[0.1] hover:shadow-xl
+                opacity-70 
+                hover:opacity-100 
+                transition ease-in-out
+                text-lg
+              ">Option</button>
+              <button className="
+                m-2 p-1 
+                outline inline-block outline-slate-100 rounded-lg 
+                hover:bg-stone-200/[0.1] hover:shadow-xl
+                opacity-70 
+                hover:opacity-100 
+                transition ease-in-out
+                text-lg
+              ">Option</button>
+              <button className="
+                m-2 p-1 
+                outline outline-slate-100 rounded-lg 
+                hover:bg-stone-200/[0.1] hover:shadow-xl
+                opacity-70 
+                hover:opacity-100 
+                transition ease-in-out
+                text-lg 
+              ">Option</button> 
             </div>
             <p>{props.txt}</p>
+          </div>
+          <div className="lg:w-1/3">
+         
           </div>
           <div className="md:flex items-center gap-5">
             <div></div>
