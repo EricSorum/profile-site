7b62feda9153043e3f992a28cc25a46c597d9441
Eric Sorum <ejsorum@gmail.com>
2025-06-09 13:34:57 -0500

    Footer mobile responsiveness.

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -1,6 +1,6 @@
 export default function Footer() {
   return (
-    <div className="flex items-center justify-between h-18 w-full bg-stone-600 mt-12">
+    <div className="flex flex-col md:flex-row items-center justify-between h-18 w-full bg-stone-600 mt-12">
       <div className="m-6">
         <ul className="flex align-center gap-3">
           <li className="opacity-70 hover:opacity-100 transition ease-in-out"><a href="/">Privacy Policy</a></li>
