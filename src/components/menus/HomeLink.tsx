5a889fc3fae56b99137ca97fb770c5da79f4332e
Eric Sorum <ejsorum@gmail.com>
2025-06-09 22:22:26 -0500

    Added shutter img alt.

--- src/components/menus/HomeLink.tsx
+++ src/components/menus/HomeLink.tsx
@@ -8,7 +8,7 @@ export default function HomeLink() {
       w-full flex justify-center 
       shadow-slate-200 text-3xl z-9
     ">
-      <a href="/" title="Home"><img src={shutter} ></img></a>
+      <a href="/" title="Home"><img src={shutter} alt="Shutter image for home link."></img></a>
     </div>
   )
 }
