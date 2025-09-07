c2e2cd114501db4d552f68bec8962ded8ed5672c
Eric Sorum <ejsorum@gmail.com>
2025-09-07 15:54:06 -0500

    Added sr-only class.

--- src/components/menus/HomeLink.tsx
+++ src/components/menus/HomeLink.tsx
@@ -8,7 +8,9 @@ export default function HomeLink() {
       w-full flex justify-center 
       shadow-slate-200 text-3xl z-9
     ">
-      <a href="/" title="Home"><img src={shutter} alt="Shutter image for home link."></img></a>
+      <a href="/" title="Home" className="focus:outline-none focus:ring-2 focus:ring-white focus:ring-opacity-50 rounded">
+        <img src={shutter} alt="Shutter image for home link."></img>
+      </a>
     </div>
   )
 }
