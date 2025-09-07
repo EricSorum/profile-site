2cffe6d3aee74e63131f844f3208b9885cad6257
Eric Sorum <ejsorum@gmail.com>
2025-09-07 15:52:37 -0500

    Replaced div with p

--- src/components/LowerLeftText.tsx
+++ src/components/LowerLeftText.tsx
@@ -1,7 +1,7 @@
 export default function LowerLeftText() {
   return (
-    <div className="absolute bottom-[10px] left-[10px] text-right shadow-slate-200 text-3xl text-shadow">
+    <p className="absolute bottom-[10px] left-[10px] text-right shadow-slate-200 text-3xl text-shadow">
       See your vision come to life.
-    </div>
+    </p>
   )
 }
